a9b0efda962bedc22de2b276e953d5b09c864c0a
eytienne-esiee <etienne.arabeyre@edu.esiee.fr>
2019-09-03 14:24:46 +0200

    milieu premier tp

---
+++ format1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+	int variable = 20;
+	printf("%d\n", variable);
+}
---
+++ interact1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(){
+	int* age;
+	printf("Quel age ?\n");
+	scanf("%d", age);
+	printf("Vous avez %d an(s)\n", age);
+}
