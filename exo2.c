4216be2053981cb88c2492d3272a3f22438399b2
eytienne-esiee <etienne.arabeyre@edu.esiee.fr>
2019-09-16 08:28:19 +0200

    fin premier tp en fi 2 (correction index)

--- exo2.c
+++
@@ -1,7 +0,0 @@
-int x ; int y=1 ;
-/* x déclaré ; y initialisé */
-x=4; // x affecté
-x=y; // x réaffecté
-
-// x == 1
-// y == 1
--- format2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	double x = 42.2141521;
-	printf("%.2f\n", x);
-}
